510ff5c31c00d56541d08684d6b7d6c8c4499d21
lbauch <bauchspiess.lucas@gmail.com>
2024-12-31 16:43:22 -0300

    Introdução js

--- auxiliares/funcoes_importantes.js
+++ auxiliares/funcoes_importantes.js
@@ -107,6 +107,17 @@ for(a = 0; a < x; a++){
     document.write('<br> O valor de a é ' + a);
 }
 
+// for iterator
+lista = ['a', 'b', 'c', 'd'];
+// imprime o índice
+for(let letra in lista){
+    console.log(letra);
+}
+// imprime o valor correspondente ao índice
+for(let letra in lista){
+    console.log(lista[letra]);
+}
+
 // switch
 switch(x){
     case 0:
@@ -119,7 +130,7 @@ switch(x){
         alert('x vale 2');
         break;
     default:
-        alert('x é maior que 2, menor que 0 ou não é número')
+        alert('x é maior que 2, menor que 0 ou não é número');
 }
 
 // --- intervalos de tempo
@@ -139,4 +150,103 @@ clearInterval(tempo);
 // setTimeout() - executa a ação do 1º param após o tempo definido no 2° param
 setTimeout(acao, 3000);
 
+// ------ WebStorage
 
+// Salvar variável local
+// localStorage.setItem() - recebe uma tupla chave, valor e salva como "cookie"
+localStorage.setItem('nome', 'Lucas');
+// Outra forma: - cria caso não exista ou reescreve caso já exista
+localStorage.nome = 'Matheus';
+
+//obter valor armazenado
+// localStorage.getItem() - recebe como parâmetro o nome da chave e retorna o valor correspondente
+localStorage.getItem('nome');
+// Outra forma: - retorno o valor armazenado na variável
+localStorage.nome;
+
+// remover valor de variável local pelo nome da chave passado como parâmetro
+localStorage.removeItem('nome');
+
+
+// Comparação de tipos - TYPEOF
+if (typeof localStorage.nome == 'undefined'){
+    localStorage.nome = prompt('Digite seu nome:');
+}
+nome = localStorage.nome;
+document.getElementById('nome').innerHTML = nome;
+
+// Também é possível salvar uma lista no localStorage ou sessionStorage:
+localStorage.lista = ['valor1', 'valor2', 3, 5.4, nome];
+
+/*
+sessionStorage - utilizado da mesma forma que o localStorage, porém é automaticamente removido
+ao fechar, atualizar ou reiniciar o navegador
+*/
+sessionStorage.lista = ['valor1', 'valor2', 3, 5.4, nome];
+
+
+// ------ VARIÁVEIS
+
+// utilizando var, let ou const
+
+// VAR - ao ser criado dentro de if ou loop, pode ser utilizado fora do mesmo:
+if (true) {
+    var teste = 3;
+}
+alert(teste);
+
+// LET - ao ser criado dentro de if ou loop, não pode ser utilizado fora do mesmo:
+// Somente pode ser utilizado dentro do escopo ao qual pertence
+// Caso tenha sido criado no main do código, pode ser utilizado como o var
+if (true) {
+    let valor_let = 3;
+}
+alert(valor_let);
+
+// CONST - variável com valor imutável
+const valor_const = 5.7;
+// ao tentar executar o código abaixo, resulta em erro
+valor_const = 6;
+
+// Concatenar strings com valores: - utiliza-se entre crases
+alert(`o valor escrito é ${valor_const}`);
+
+// -----DESCONTRUINDO UMA VÁRIÁVEL OU LISTA:
+const pessoa = {
+    nome : 'Lucas',
+    sobrenome : 'Bau',
+    idade: 30,
+    cargo: 'profissional'
+};
+
+// Cria uma variável com o nome informado e atribui a ela o valor da respectiva chave do dicionário passado
+let {nome} = pessoa;
+let {sobrenome} = pessoa;
+let {idade, cargo} = pessoa;
+
+// Cria uma variável com o nome passado após os : e atribui a ela o valor da chave antes dos :
+// Neste caso, seria o mesmo que: profissao = cargo, considerando que cargo foi criado na linha acima.
+let {cargo:profissao} = pessoa;
+
+/*
+Ao desconstruir uma variável, cria-se uma nova. Então, ao alterar o valor da nova variável,
+a antiga variável não é afetada.
+por exemplo:
+sobrenome = 'Algum Sobrenome' ou 
+profissao = 'Alguma Profissão'
+Ao fazer isso, a variável pessoa se mantém com os dados originais
+*/
+
+// Desconstruindo a lsta:
+let nomes = ['nome1', 'nome2', 'nome3'];
+
+// fazendo a desconstrução:
+// cria a variável segundo_nome e atribui a ela o valor de nomes[1]
+let {1:segundo_nome} = nomes;
+// pegando mais de um valor simultaneamente:
+let {2:terceiro_nome, 0:primeiro_nome} = nomes;
+// outra forma:
+let [prim_nome, seg_nome, terc_nome] = nomes
+// não precisa conter todos os elementos, mas, as variáveis recebem sempre na ordem, sem pular elementos
+// neste caso, atribui às duas variáveis os 2 primeiros valores
+let [nome_1, nome_2] = nomes
---
+++ auxiliares/script_teste.js
@@ -0,0 +1,4 @@
+const valor_const = 5.7
+
+// Concatenar strings com valores: - utiliza-se entre crases
+alert(`o valor escrito é ${valor_const}`)
